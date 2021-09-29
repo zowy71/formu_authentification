292be78fe7cc7af69ae2ded0794c26c963dd10e8
perr0112 <clement.perrot@etudiant.univ-reims.fr>
2021-09-29 16:50:45 +0000

    Add new file

---
+++ auth1.php
@@ -0,0 +1,24 @@
+<?php declare(strict_types=1);
+
+require_once('autoload.php');
+
+$authentication = new UserAuthentication();
+
+$p = new WebPage('Authentification');
+
+try {
+    // Tentative de connexion
+    $user = $authentication->getUserFromAuth();
+    $p->appendContent(<<<HTML
+<div>Bonjour {$user->getFirstName()}</div>
+HTML
+    );
+} catch (AuthenticationException $e) {
+    // Récupération de l'exception si connexion échouée
+    $p->appendContent("Échec d'authentification&nbsp;: {$e->getMessage()}");
+} catch (Exception $e) {
+    $p->appendContent("Un problème est survenu&nbsp;: {$e->getMessage()}");
+}
+
+// Envoi du code HTML au navigateur du client
+echo $p->toHTML();
