aa595d9a4cbb34600d274688f181aaf5a3a65a3f
perr0112 <clement.perrot@etudiant.univ-reims.fr>
2021-10-16 21:02:26 +0000

    Update form1.php

--- form1.php
+++ form1.php
@@ -4,12 +4,10 @@ declare(strict_types=1);
 require_once('autload.php');
 require_once('MyPD0.php');
 
-// Création de l'authentification
 $authentication = new UserAuthentication();
 
 $p = new WebPage('Authentification');
 
-// Production du formulaire de connexion
 $p->appendCSS(<<<CSS
     form input {
         width : 4em ;
