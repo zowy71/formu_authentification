b026da6765b73d3f2a9004865a54635e3593cd3c
perr0112 <clement.perrot@etudiant.univ-reims.fr>
2021-10-16 20:40:13 +0000

    Add new file

---
+++ form2.php
@@ -0,0 +1,33 @@
+<?php
+require_once('autload.php');
+require_once('src/MyPD0.php');
+
+$p = new WebPage('Authentification');
+
+// Production du formulaire de connexion
+$p->appendCSS(
+  <<<CSS
+    form input {
+        width : 4em ;
+    }
+CSS
+);
+
+if (User::isConnected()) {
+  $form = User::logoutForm();
+  $p->appendContent(
+    <<<HTML
+    {$form}
+HTML
+  );
+}
+else {
+  $form = User::loginForm('auth1.php');
+  $p->appendContent(
+    <<<HTML
+    {$form}
+HTML
+  );
+}
+
+echo $p->toHTML();
